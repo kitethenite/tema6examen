73c9ea123ff3c9080ff98148376fe1e323c4eadd
Nicole Heywood <nicsayhey@gmail.com>
2024-02-05 21:42:09 +0100

    commited changes after exam period

--- js/animations.js
+++ js/animations.js
@@ -1,29 +1,29 @@
 // animation för ux-principer
 
-// let observer = new IntersectionObserver (entries => {
-//     entries.forEach(entry => {
-//         if (entry.isIntersecting) {
-//             entry.target.classList.add("design-principles");
-//             return; // detta ger ett avslut på funnktionen då den är färdig efter att ha lagt till en class.
-//         }
-//         entry.target.classList.remove("design-principles"); // tar bort classen från elementet när utanför viewport
-//     });
-// });
+let observer = new IntersectionObserver (entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("design-principles");
+            return; // detta ger ett avslut på funnktionen då den är färdig efter att ha lagt till en class.
+        }
+        entry.target.classList.remove("design-principles"); // tar bort classen från elementet när utanför viewport
+    });
+});
 
-// observer.observe(document.querySelector(".ux-animations"));
+observer.observe(document.querySelector(".ux-animations"));
 
 
-// let observer2 = new IntersectionObserver(entries => {
-//     entries.forEach(entry => {
-//         if (entry.isIntersecting) {
-//             entry.target.classList.add("ux-similarities");
-//         } else {
-//             entry.target.classList.remove("ux-similarities");
-//         }
-//     });
-// });
+let observer2 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("ux-similarities");
+        } else {
+            entry.target.classList.remove("ux-similarities");
+        }
+    });
+});
 
-// observer2.observe(document.querySelector(".ux-animations2"));
+observer2.observe(document.querySelector(".ux-animations2"));
 
 
 
